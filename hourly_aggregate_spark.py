728cc9b919b401187b2c8cd1c1fee7366084242f
Suhas <suhas.hm109@gmail.com>
2015-09-22 19:22:21 -0400

    Updated Batch processing code to add to cassandra

--- hourly_aggregate_spark.py
+++ hourly_aggregate_spark.py
@@ -3,10 +3,20 @@ import json
 #import extract_time
 
 import datetime
-def get_unix_time(ctime):
+def write_to_cassandra(input):
+    from cassandra.cluster import Cluster
+    cluster = Cluster(['52.20.47.196'])
+    session = cluster.connect('playground')
+    stmt = "INSERT INTO test_hourly2 (event_time, garage_name, availability) VALUES (%s, %s,%s)"
+    #print input[0][1]
+    session.execute(stmt, parameters=[str(input[0][0]), input[0][1], str(input[1])])
+    return input[0][1]
 
-    # convert ctime to unix timestamp
+def get_unix_time(ctime):
     time_list = ctime.split()
+    # ctime 'Tue Sep 15 15:16:58 2015'
+
+    # convert to ctime
     time_list = time_list[ :-2]
 
     temp = time_list[-1]
@@ -27,14 +37,14 @@ def create_tuple(r):
     if '_geofire' in garages:
         garages.pop('_geofire')
     for i in garages:
-        #print i
-        res.append(((formatted_time, i.replace(" ","_")), garages[i]['open_spaces']))
+	#print i
+        res.append(((int(formatted_time), i.replace(" ","_").lower()), garages[i]['open_spaces']))
     return res
 
 conf = SparkConf().setAppName("hourly-stats")
 sc = SparkContext(conf=conf)
 parking_json = sc.textFile("hdfs://ec2-52-3-61-194.compute-1.amazonaws.com:9000/camus/topics/park/hourly/2015/09/15/08/park.0.0.10.10.1442329200000.gz")
 formatted_data = parking_json.flatMap(lambda s: create_tuple(s)).reduceByKey(lambda a,b: (int(a)+int(b))/2)
-#formatted_data = parking_json.flatMap(lambda s: create_tuple(s))
+write_to_db = formatted_data.map(lambda line: write_to_cassandra(line))
 
-print formatted_data.take(4)
+print 'Total records', write_to_db.count()
